c0e34a48f760663bc4e0634a6ead3c28d1b3c09f
Kirill <lukashink@ae.com>
2021-04-24 21:15:29 +0100

    Added delete tests. Style fixes.

--- StorageService.test.js
+++ StorageService.test.js
@@ -2,30 +2,38 @@ const StorageService = require('./StorageService');
 const storage = new StorageService();
 
 class Pair {
-    constructor(value1, value2) {
-        this.field1 = value1
-        this.field2 = value2
+    constructor(key, value) {
+        this.key = key;
+        this.value = value;
     }
 }
 
-let key1 = "key1"
-let key2 = "key2"
-let value0 = "value0"
-let value1 = "value1"
-let value2 = "value2"
+let key1 = "key1";
+let key2 = "key2";
+let value0 = "value0";
+let value1 = "value1";
+let value2 = "value2";
+let value3 = "value3";
 
-let pair1 = new Pair(key1, value1)
-let pair2 = new Pair(key2, value2)
+let pair1 = new Pair(key1, value1);
+let pair2 = new Pair(key2, value2);
 
 let id0 = storage.add(value0);
 let id1 = storage.add(pair1);
 let id2 = storage.add(pair2);
+let id3;
 let notExistId = '100';
-let wrongIdType = 2
+let wrongIdType = 2;
+
+test('add test', () => {
+    id3 = storage.add(value3);
+    expect(typeof id3).toBe('string')
+    expect(id3).toBe(String(Number(id2) + 1));
+});
 
 test('get by id', () => {
-    let elementById = storage.getById(id0);
-    expect(elementById).toBe(value0);
+    let elementById = storage.getById(id3);
+    expect(elementById).toBe(value3);
 });
 
 test('get pair by id', () => {
@@ -47,3 +55,20 @@ test('get by wrong id type', () => {
 test('get by non exist id', () => {
     expect(storage.getById(notExistId)).toBeUndefined();
 });
+
+test('delete by id', () => {
+    expect(storage.deleteById(id1)).toBeTruthy();
+    expect(storage.getById(id1)).toBeUndefined();
+});
+
+test('delete by wrong id', () => {
+    expect(storage.deleteById(notExistId)).toBeFalsy();
+});
+
+test('delete by wrong id', () => {
+    expect(storage.deleteById(wrongIdType)).toBeFalsy();
+});
+
+test('log get all', () => {
+    console.log(storage.getAll())
+})
